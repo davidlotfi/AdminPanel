49eb4f0c66d841e1fd856eaf0a6545be58a469a9
davidlotfi <boukernineabdellatif@gmail.com>
2021-09-03 00:23:15 +0200

    up file

--- login.php
+++ login.php
@@ -1,21 +1,35 @@
 <?php
-include_once 'include/layout.php';
-require_once 'include/Connexion.php';
+ require_once 'controller/login_post.php';
  ?>
-
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <title>SB Admin - Start Bootstrap Template</title>
+   <!-- Bootstrap core CSS-->
+   <link href="css/bootstrap.min.css" rel="stylesheet">
+   <!-- Custom fonts for this template-->
+   <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   <!-- Custom styles for this template-->
+   <link href="css/sb-admin.css" rel="stylesheet">
+ </head>
 <body class="bg-dark">
   <div class="container">
     <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
+      <div class="card-header">HabbidouPanel</div>
       <div class="card-body">
-        <form>
+        <form action="login.php" method="post">
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input class="form-control" name="_email" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email" required>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
+            <input class="form-control" name="_password" id="exampleInputPassword1" type="password" placeholder="Password" required>
           </div>
           <div class="form-group">
             <div class="form-check">
@@ -23,11 +37,10 @@ require_once 'include/Connexion.php';
                 <input class="form-check-input" type="checkbox"> Remember Password</label>
             </div>
           </div>
-          <a class="btn btn-primary btn-block" href="index.php">Login</a>
+          <input class="btn btn-primary btn-block" type="submit" name="commit" value="Login">
         </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="register.php">Register an Account</a>
-          <a class="d-block small" href="forgot-password.php">Forgot Password?</a>
+        <div class="text-center"><br>
+          <p class="d-block small">habbidou.com</p>
         </div>
       </div>
     </div>
