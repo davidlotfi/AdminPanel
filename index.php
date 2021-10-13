03e5e8ab2e23c5955a3fa859842c2b3011673b18
davidlotfi <boukernineabdellatif@gmail.com>
2021-10-13 14:55:14 +0200

    style

--- index.php
+++ index.php
@@ -78,12 +78,16 @@ $nombres_users = count_all_users();
                            ?>
                        <tr>
                          <td><?php echo htmlspecialchars($donnes['description']); ?></td>
-                         <td><img class="img-thumbnail hh" src="imagesProd/<?php if(empty($donnes['image'])){echo "image.png";}else{ echo htmlspecialchars($donnes['image']);} ?>" alt=""></td>
+                         <td>
+                           <a href="view.php?IDD_Prod=<?php echo htmlspecialchars($donnes['id_produit']); ?>">
+                           <img class="img-thumbnail hh" src="imagesProd/<?php if(empty($donnes['image'])){echo "image.png";}else{ echo htmlspecialchars($donnes['image']);} ?>" alt="">
+                           </a>
+                         </td>
                          <td><?php echo htmlspecialchars($donnes['prix']); ?></td>
                          <td>0<?php echo htmlspecialchars($donnes['telephone']); ?></td>
                          <td><?php echo htmlspecialchars($donnes['date']); ?></td>
                          <td><div class="btn-group btn-group-toggle" data-toggle="buttons">
-                              <label class="btn btn-secondary active">
+                              <label class="btn btn-secondary">
                                 <input type="radio" name="options" id="option1" autocomplete="off" checked> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
                                 <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
                                 <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
